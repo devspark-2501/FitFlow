854d6467ad691aa55ec95ef22b162c644fbe869e
Tanush Mathur <devspark2501@gmail.com>
2026-09-08 22:23:15 +0530

    Code push

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,5 +1,5 @@
+import 'package:fitflow/screens/auth/register_screen.dart';
 import 'package:flutter/material.dart';
-import 'register_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
