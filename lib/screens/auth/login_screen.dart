149b72447b28c099f4e810e50c6b67ee2d442abe
Tanush Mathur <devspark2501@gmail.com>
2026-09-09 21:19:19 +0530

    Login_screen.dart

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
+import 'package:fitflow/screens/%20profile/profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import '../profile/profile_screen.dart';
+// import '../profile/profile_screen.dart';
 import 'register_screen.dart';
 
 class LoginScreen extends StatefulWidget {
