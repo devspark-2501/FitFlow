9754598055d8d97e072ff57e545483b04832686e
Tanush Mathur <devspark2501@gmail.com>
2026-09-11 14:31:18 +0530

    Resgister_screen added

--- lib/screens/auth/register_screen.dart
+++ lib/screens/auth/register_screen.dart
@@ -66,7 +66,7 @@ class _RegisterScreenState extends State<RegisterScreen>
 
     try {
       final response = await http.post(
-        Uri.parse('http://localhost:5000/api/auth/register'),
+        Uri.parse('http://127.0.0.1:5000/api/auth/register'),
         headers: {'Content-Type': 'application/json'},
         body: jsonEncode({
           'name': name,
