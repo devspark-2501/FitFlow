f55c392d03a058210fda0f92ca96efc2f5e54a18
Tanush Mathur <devspark2501@gmail.com>
2026-09-12 20:40:49 +0530

    Regsiter_screen.dart

--- lib/screens/auth/register_screen.dart
+++ lib/screens/auth/register_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
-import 'package:fitflow/screens/profile/profile_screen.dart';
+import 'package:fitflow/screens/%20profile/profile_screen.dart';
+// import 'package:fitflow/screens/profile/profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
