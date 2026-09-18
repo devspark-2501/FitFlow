0e40cf818650de5155fec9df4d6ef5f68871e496
Tanush Mathur <devspark2501@gmail.com>
2026-09-18 20:09:31 +0530

    Server.js updated with updgradtion to profile route

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
-app.use('/api/water', require('./routes/water')); // Added water route
+app.use('/api/water', require('./routes/water'));
 
 const PORT = process.env.PORT || 5000;
 
